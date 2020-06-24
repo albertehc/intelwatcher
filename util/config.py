2321e957f1176002370ad4097890cfd273c6d77d
ccev <ccev>
2020-06-24 14:29:06 +0200

    custom cookie text

--- util/config.py
+++ util/config.py
@@ -7,8 +7,9 @@ class create_config:
 
         self.bbox = config_file.get("Config", "bbox")
         self.cookie_wh = config_file.getboolean("Config", "cookie_webhooks")
+        self.cookie_wh = config_file.get("Config", "custom_cookie_text", fallback="")
         self.wh_url = config_file.get("Config", "webhook_url")
-        self.workers = config_file.getint("Config", "workers")
+        self.workers = config_file.getint("Config", "workers", fallback=1)
 
         self.scan_type = config_file.get("DB", "scanner").lower()
         self.db_name_scan = config_file.get("DB", "scanner_db_name")
