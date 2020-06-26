50d59a9a649b41a2492007fb688fe0d588ba1774
Clayton Burlison <git@clburlison.com>
2020-06-25 23:58:56 -0500

    fix webhook alerts and custom cookie txt

--- util/config.py
+++ util/config.py
@@ -7,7 +7,7 @@ class create_config:
 
         self.bbox = config_file.get("Config", "bbox")
         self.cookie_wh = config_file.getboolean("Config", "cookie_webhooks")
-        self.cookie_wh = config_file.get("Config", "custom_cookie_text", fallback="")
+        self.cookie_text = config_file.get("Config", "custom_cookie_text", fallback="")
         self.wh_url = config_file.get("Config", "webhook_url")
         self.workers = config_file.getint("Config", "workers", fallback=1)
 
